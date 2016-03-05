8fa12e8fe1dd15c3eeed5bf8274b5ca66cd31f8d
Alan <371725153@qq.com>
2016-03-05 16:12:30 +0800

    day6 冒险

---
+++ day6 adventure/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#antuor:Alan
+from adventure import initialize_game
+from adventure import play_game
+
+###################主函数########################
+if __name__ == "__main__":
+
+    initialize_game()
+    play_game()
+
+    exit()
