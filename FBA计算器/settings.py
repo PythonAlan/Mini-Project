ba7cce1f2883840bcd8ac1c5c100dd2dc7e3e0ad
Alan <371725153@qq.com>
2016-03-13 00:33:32 +0800

    fab calculator

---
+++ FBA计算器/settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#antuor:Alan
+
+
+fee_rate = {
+    'commissionRate':0.15,
+    'packageCost' : 0.4,
+    'fbaFee': 4.40,
+    'damageRate':0.03,
+    'firstClass':0.035,
+    'toFba': 1,
+    'exchangeRate': 6.1
+
+}
